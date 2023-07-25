6fe116409a5f1b8b0efd48778ee254926ed1e5f9
Eduard Rytslin <Emillen98@yandex.ru>
2023-07-25 15:36:30 +0300

    Написаны программы по задачам

---
+++ HomeWork/Task58/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] array1 = CreateMatrixRndInt(2, 2, 1, 10);
+int[,] array2 = CreateMatrixRndInt(2, 2, 1, 10);
+int[,] multipl = Multiplication(array1, array2);
+
+Console.WriteLine("Матрица 1: ");
+PrintMatrix(array1);
+Console.WriteLine("Матрица 2: ");
+PrintMatrix(array2);
+Console.WriteLine("Произведение этих двух матриц равно: ");
+PrintMatrix(multipl);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] Multiplication (int[,] matrix1, int[,] matrix2)
+{
+    int[,] MultiplMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            for (int r = 0; r < matrix2.GetLength(0); r++)
+            {
+            MultiplMatrix[i, j] += matrix1[i, r] * matrix2[r, j];
+            }
+        }
+    }
+    return MultiplMatrix;
+}
---
+++ HomeWork/Task60/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// Результат:
+// 66(0,0,0) 25(0,1,0) 27(0,0,1) 90(0,1,1)
+// 34(1,0,0) 41(1,1,0) 26(1,0,1) 55(1,1,1)
+
+int[,,] array3D = CreateMatrix3DRndInt(2, 2, 2, 10, 99);
+LinePrint(array3D);
+
+int[,,] CreateMatrix3DRndInt(int rows, int columns, int width, int min, int max)
+{
+    int[,,] matrix3D = new int[rows, columns, width];
+    
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                matrix3D[i, j, k] = rnd.Next(min, max + 1);
+            }
+        }
+    }
+    return matrix3D;
+}
+
+void LinePrint(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++ )
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i,j,k]}({i},{j},{k}) ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
